61e3bcc2d5ff17fb88fbbee1ee1700f53af4e152
Nilay K <nilayk111@gmail.com>
2022-02-04 23:27:14 +0530

    Update nilay_code.py

--- nilay_code.py
+++ nilay_code.py
@@ -1,5 +1,113 @@
-from copy import deepcopy
 
+#file1 = input file     file2=output file
+file1 = open(r"C:\Users\nk\Desktop\python\a_an_example.in.txt")
+file2 = open(r"C:\Users\nk\Desktop\python\example.txt","w")
+
+#data structures to store the input
+customer =dict()
+item = dict()
+
+cases = int(file1.readline())
+
+#iterate over each customer
+for i in range(cases):
+    likes = file1.readline().rstrip().split()[1:]
+    dislikes = file1.readline().rstrip().split()[1:]
+    
+    #store likes and dislikes of that customer
+    customer[i+1]=[likes,dislikes]
+
+    #for each liked item, store that customer(that is, i+1) in item dictionary item[][0]
+    for val in likes:
+        if val in item:
+            item[val][0].append(i+1)
+        else:
+            item[val]=[[i+1],[]]
+    
+    #for each disliked item, store that customer(that is, i+1) in item dictionary item[][1]
+    for val in dislikes:
+        if val in item:
+            item[val][1].append(i+1)
+        else:
+            item[val]=[[],[i+1]]
+
+#main conflict matrix to represent all the incompatible customers
+conflict=dict()
+conflict_size=dict()
+
+#go through each customer, creating a list of conflicts for that customer
+for person in customer:
+    conflict[person]=set()
+    
+    #compare this customer's disliked items against liked items of all the other customers
+    for disliked_item in customer[person][1]:
+        for val in item[disliked_item][0]:
+            if val != person:
+                conflict[person].add(val)
+                
+    #compare this customer's liked items against disliked items of all the other customers
+    for liked_item in customer[person][0]:
+        for val in item[liked_item][1]:
+            if val!= person:
+                conflict[person].add(val)
+    
+    #store the number of conflicts for each customer
+    conflict_size[person]=len(conflict[person])
+
+
+print('conflict matrix',conflict)
+print('conflict_size',conflict_size)
+
+
+#main algo to calculate the result which is a list of all compatible customers
+result=[]
+while(conflict_size):
+    min_val=10000
+
+    #find the customer with minimum conflicts
+    for k,v in conflict_size.items():
+        if v < min_val:
+            min_val=v
+            min_key=k
+    #add that customer to the result
+    result.append(min_key)
+
+    #delete that customer from the conflict matrix
+    del conflict_size[min_key]
+
+    #remove all the customers that had a conflict with the added customer, as they
+    #cannot be in the result anymore
+    for cust in conflict[min_key]:
+        flag=conflict_size.pop(cust,-1)
+
+        #For all the customers that were just removed, remove their effect on the result of the customers
+        #If any other customers had a conflict with the removed ones, reduce that number
+        if flag!=-1:
+            for temp_cust in conflict[cust]:
+                if temp_cust in conflict_size.keys():
+                    conflict_size[temp_cust]-=1
+    
+
+#Now that we have all the customers in result, check which ingredients they want and that is the answer
+ingredients = set()
+for cust in result:
+    for ingredient in customer[cust][0]:
+        ingredients.add(ingredient) 
+
+#store the final result (count & ingredients) in the output file
+count = len(ingredients)
+print(count,' '.join(ingredients),file=file2)
+print('this is the final answer',result)
+
+#close the files
+file1.close()
+file2.close()
+
+
+
+
+
+#recursive approach -- optimal solution -- IGNORE
 """ to be used
 def next_choice(conflict_size,next):
     print('entered function')
@@ -41,108 +149,3 @@ def next_choice(conflict_size,next):
     return ans
 
 """
-    #print('discarded conflict',myconflict)
-    #min_len = min(copy_conflict.values(),key=len)
-    #print('min_len',min_len)
-    #min_conflicts = [i for i, val in copy_conflict.items() if len(val) == len(min_len)]
-    #print('next value: ',next,'min_conficts: ',min_conflicts)
-    #print('min_conflicts inside function..',min_conflicts)
-    # result=[]
-    # for val in min_conflicts:
-    #     temp = next_choice(copy_conflict,val)
-    #     #print('this is temp...',temp)
-    #     if len(temp) > len(result):
-    #         result = temp
-    #         #print('assigned result...',result)
-    # #print('outside of for loop...',result)
-    # ans.extend(result)
-    # #print('this is returning value...',ans)
-    # return ans
-    
-
-
-file1 = open(r"C:\Users\nk\Desktop\python\a_an_example.in.txt")
-file2 = open(r"C:\Users\nk\Desktop\python\example.txt","w")
-customer =dict()
-item = dict()
-cases = int(file1.readline())
-for i in range(cases):
-    likes = file1.readline().rstrip().split()[1:]
-    dislikes = file1.readline().rstrip().split()[1:]
-    #print('likes',likes,'dislikes',dislikes)
-    customer[i+1]=[likes,dislikes]
-    for val in likes:
-        if val in item:
-            item[val][0].append(i+1)
-        else:
-            item[val]=[[i+1],[]]
-    for val in dislikes:
-        if val in item:
-            item[val][1].append(i+1)
-        else:
-            item[val]=[[],[i+1]]
-#print('customer',customer)
-#print('item',item)
-
-conflict=dict()
-conflict_size=dict()
-#print('before',conflict_size)
-for person in customer:
-    conflict[person]=set()
-    for disliked_item in customer[person][1]:
-        for val in item[disliked_item][0]:
-            if val != person:
-                print(disliked_item,val)
-                conflict[person].add(val)
-    for liked_item in customer[person][0]:
-        for val in item[liked_item][1]:
-            if val!= person:
-                conflict[person].add(val)
-    conflict_size[person]=len(conflict[person])
-print('conflict matrix',conflict)
-print('conflict_size',conflict_size)
-result=[]
-while(conflict_size):
-    min_val=10000
-    for k,v in conflict_size.items():
-        if v < min_val:
-            min_val=v
-            min_key=k
-    result.append(min_key)
-    del conflict_size[min_key]
-    for cust in conflict[min_key]:
-        #print('cust ',cust)
-        flag=conflict_size.pop(cust,-1)
-        if flag!=-1:
-            for temp_cust in conflict[cust]:
-                if temp_cust in conflict_size.keys():
-                    #print('temp_cust ',temp_cust)
-                    conflict_size[temp_cust]-=1
-    
-
-
-# min_len = min(conflict_size.values())
-# min_conflicts = [i for i, val in conflict_size.items() if val == min_len]
-
-# #print('min_len',min_len)
-# #print('min_conflicts',min_conflicts)
-# #min_conflicts = sorted(conflict.values(),key=len)
-
-# result = []
-# for test in min_conflicts:
-# #     #print('working on..',val,conflict)
-#     temp_result= next_choice(conflict_size,test)
-#     if len(temp_result) > len(result):
-#         result=temp_result
-
-#print('this is the result: ',result)
-ingredients = set()
-for cust in result:
-    for ingredient in customer[cust][0]:
-        ingredients.add(ingredient) 
-count = len(ingredients)
-print(count,' '.join(ingredients),file=file2)
-print('this is the final answer',result)
-file1.close()
-file2.close()
-#print('final list of customers: ',result)
